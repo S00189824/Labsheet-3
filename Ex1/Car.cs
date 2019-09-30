593bfc0cdff67e21381cd598a0738ad3d18405b5
David Coleman <46888854+S00189824@users.noreply.github.com>
2019-09-30 13:00:34 +0100

    Add project files.

---
+++ Ex1/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    class Car
+    {
+
+        private string make;
+
+        public string Make
+        {
+            get { return this.make; }
+            set { this.make = value; }
+        }
+
+        private string model;
+
+        public string Model
+        {
+            get { return this.model; }
+            set { this.model = value; }
+        }
+
+        private int currentSpeed;
+
+        public int CurrentSpeed
+        {
+            get { return this.currentSpeed; }
+            set { this.currentSpeed = value; }
+        }
+    }
+
+ 
+}
---
+++ Ex1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car c1 = new Car();
+
+            Car c2 = new Car();
+
+            c1.Make = "Ford";
+
+            Console.WriteLine(c1.Make);
+
+
+
+
+
+        }
+    }
+}
