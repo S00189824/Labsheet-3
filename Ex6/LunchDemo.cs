e753f1ec60c17f5b55691a00cf2301c805c8cfef
David Coleman <S00189824@mail.itsligo.ie>
2019-10-04 14:13:57 +0100

    Ex6 finished

---
+++ Ex6/LunchDemo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex6
+{
+    class LunchDemo
+    {
+        public string Entree { get; set; }
+        public string Side { get; set; }
+        public string Drink { get; set; }
+
+        public static void table(params LunchDemo[] lunches)
+        {
+            foreach (LunchDemo meal in lunches)
+            {
+                Console.WriteLine(meal.Entree);
+                Console.WriteLine(meal.Side);
+                Console.WriteLine(meal.Drink);
+            }
+        }
+
+    }
+}
---
+++ Ex6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LunchDemo L1 = new LunchDemo();
+
+            L1.Entree = "hamburger";
+            L1.Side = "Fries";
+            L1.Drink = "Cola";
+
+            LunchDemo L2 = new LunchDemo();
+
+            L2.Entree = "hotdog";
+            L2.Side = "chips";
+            L2.Drink = "lemonade";
+
+            LunchDemo L3 = new LunchDemo();
+
+            L3.Entree = "pizza";
+            L3.Side = "salad";
+            L3.Drink = "iced tea";
+
+            LunchDemo.table(L1, L2, L3);
+        }
+    }
+}
